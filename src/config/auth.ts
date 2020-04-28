01dc26f6c4297d7e321520f273f9869a0ccd2a20
jfilipe-dev <jfilipe1511@gmail.com>
2020-04-28 10:26:07 -0300

    Added JWT

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '68a877e64f59d874b5c8c4141bb35ecd',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcryptjs';
 import { getRepository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '68a877e64f59d874b5c8c4141bb35ecd', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
